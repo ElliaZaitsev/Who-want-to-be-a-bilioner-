499b068c8608321b8f8f7dd00b605e3cfa79ec30
who want to be a bilioner? <szaitseva02@gmail.com>
2025-10-22 18:56:05 +0200

    ahhhh

--- я не люблю координати аааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа.py
+++ я не люблю координати аааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа.py
@@ -27,7 +27,7 @@ ujustdied = [
 ]
 lolrandom = random.choice(ujustdied)
 nah = random.choice(dousomethingwin)
-heheboi = 1
+heheboi = 10
 can.geometry()
 x_size = 200
 y_size = 160
@@ -35,7 +35,6 @@ x = 860
 y = 420
 ball_x = 3
 ball_y = 3
-
 
 def buttoninbutton():
     lol1 = Label(text=nah, height=4, width=50, font=100, bg="#FFF5A8")
@@ -52,7 +51,6 @@ tipa_krasivo = "#568AD2"
 tipa_WOOW = "#79983E"
 can.resizable(True, True)
 
-
 def window():
     global ahh
 
@@ -61,14 +59,18 @@ ahh = Canvas(width=5000, height=5000, bg=tipa_zelenuj)
 ahh.pack()
 ahh.create_rectangle(0, 0, 1525, 850, fill=tipa_orange)
 plat = ahh.create_rectangle(1015, 770, 500, 740, fill=tipa_WOOW)
-ball = ahh.create_oval(126, 650, 196, 720, fill=tipa_blue)
+ball = ahh.create_oval(600, 650,680, 720, fill=tipa_blue)
+blocks=[]
 for i in range(7):
     box1 = ahh.create_rectangle(x, y, x - x_size, y + y_size, fill=tipa_janeznaju)
+    blocks.append(box1)
+
     if i == 1:
         y = 230
         x = 990
     elif i == 2:
         x = 740
+
     elif i == 3:
         x = 610
         y = 40
@@ -77,8 +79,9 @@ for i in range(7):
     elif i == 5:
         x = 1110
 
-
+print(blocks)
 def moving(event):
+
     if event.keysym == "a" and ahh.coords(plat)[0] - 20 > -5:
         ahh.move(plat, -10, 0)
     elif event.keysym == "d" and ahh.coords(plat)[2] + 20 < 1530:
@@ -101,6 +104,14 @@ def ballmove():
         width=10,
         font=100000000000000000000000000000000000000000000000000000000000000000000000000,
     )
+    if ahh.coords(ball)[2] > ahh.coords(plat)[0] and ahh.coords(ball)[2] < ahh.coords(plat)[2] \
+            and ahh.coords(ball)[3] > ahh.coords(plat)[1] and ahh.coords(ball)[3] < ahh.coords(plat)[3]:
+        ball_y=-ball_y
+        print("OLE OLE OLE OLEEE")
+    if ahh.coords(ball)[0] > ahh.coords(plat)[0] and ahh.coords(ball)[0] < ahh.coords(plat)[2] \
+            and ahh.coords(ball)[1] > ahh.coords(plat)[1] and ahh.coords(ball)[1] < ahh.coords(plat)[3]:
+        ball_y=-ball_y
+        print("OLE OLE OLE OLEEE")
     ups.place(x=1300, y=300)
     if heheboi == 1:
         ups["text"] = "❤️"
@@ -168,7 +179,22 @@ def ballmove():
         )
         lol.place(x=480, y=300)
         app2.mainloop()
-
-
+lolonly1=[1,1,1,1,1,1,1,1,1]
+def delete_blocks():
+    global lolonly1
+    if ahh.coords(ball)[0]>=860-x_size and ahh.coords(ball)[2]<=860 and ahh.coords(ball)[1]>420 and ahh.coords(ball)[3]<420+y_size:
+        ahh.delete(blocks[0])
+        lolonly1[0]=0
+        print("LOOOOOOOOOOOOL")
+    elif ball_x>=990 and ball_x<740+x_size and ball_y>40 and ball_y<40+y_size:
+        ahh.delete(blocks[1])
+        lolonly1[1]=0
+        print("EZ")
+    elif ball_x>=1110 and ball_x<860+x_size and ball_y>40 and ball_y<40+y_size:
+        ahh.delete(blocks[2])
+        lolonly1[2] = 0
+        print("she was a fairy pum pu rum rum pum pu ru rum")
 ballmove()
+ahh.after(5,delete_blocks)
+delete_blocks()
 can.mainloop()
